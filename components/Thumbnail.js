7d8a5746e2f10da3d25464af05c05562919ebc28
unknown <andreas.alfven95@gmail.com>
2021-09-07 20:26:48 +0200

    Fixed results and visuals.

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -10,6 +10,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       className='p-2 group cursor-pointer transition duration-200 ease-in sm:hover:scale-105 hover:z-50'
     >
       <Image
+        alt={result.title || result.original_name}
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
           `${BASE_URL}${result.poster_path}`
